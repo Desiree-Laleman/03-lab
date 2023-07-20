371b12676a1744cfe11bbed4fff0c3d2d327865a
Desiree <dezlaleman@gmail.com>
2023-07-20 12:00:26 -0400

    Updated Notes and more working code

--- script.js
+++ script.js
@@ -22,13 +22,10 @@ const submissions = [
   },
 ];
 
+//
 const addSubmission = (array, newName, newScore) => {
   array.push({ name: newName, score: newScore, passed: newScore >= 60 });
 };
-// addSubmission(submissions, "James", 100);
-
-// const addSubmission = (array, newName, newScore) =>
-//   array.push({ name: newName, score: newScore, passed: score >= 60 });
 // addSubmission(submissions, "James", 100);
 // console.log(submissions);
 
@@ -38,11 +35,19 @@ const deleteSubmissionByIndex = (array, index) => array.splice(index, 1);
 const deleteSubmissionByName = (array, name) => {
   const nameIndex = array.findIndex((item) => item.name === name);
   array.splice(nameIndex, 1);
+  // Below deletes multiple!
+  // change array to LET
+  // array.filter((item) => item.name !== name);
 };
 // deleteSubmissionByName(submissions, "Jack");
+// console.log(submissions);
 
 const editSubmission = (array, index, score) => {
+  // find the Index that matches the name given in the search
+  // const index = array.findIndex((item) => item.name === name)
+  // looking in the given array, at the given index, at JUST the score, will now be the new given score
   array[index].score = score;
+  // looking in the array, at the given index, at the passed area- is the score greaterthan or equel to 60? True or False.
   array[index].passed = score >= 60;
 };
 // editSubmission(submissions, 2, 80);
@@ -51,8 +56,10 @@ const findSubmissionByName = (array, name) =>
   array.find((item) => item.name === name);
 // console.log(findSubmissionByName(submissions, "Jill"));
 
-// pv = array[0] cv = array[1] if none defined
+// reducde method will only return ONE thing
+// pv = array[0] cv = array[1] if none undefined
 // loop through the array, if cv.score is less than pv.score, return cv, if not, reutrn pv
+// is current score less than previous score? if true return current : if false return previous
 const findLowestScore = (array) => {
   return array.reduce((pv, cv) => (cv.score < pv.score ? cv : pv));
   //   let currentLowest = array[0];
@@ -63,50 +70,42 @@ const findLowestScore = (array) => {
   //   });
   //   return currentLowest;
 };
+// const currentLowest = findLowestScore(array);
+// const newArray = [];
+// array.forEach((item) => item.score <= currentLowest.score)
 // console.log(findLowestScore(submissions));
 
-let sum = 0;
 const findAverageScore = (array) => {
   return array.reduce((pv, cv) => pv + cv.score, 0) / array.length;
 };
 // console.log(findAverageScore(submissions));
 
+// function filderPassing NEEDS an Array RETURN array.filter ((itemInArray) RETURN itemInArray.IFPASSEDISTRUE)
 const filterPassing = (array) => array.filter((item) => item.passed);
 // console.log(filterPassing(submissions));
 
 const filter90AndAbove = (array) => array.filter((item) => item.score >= 90);
 // console.log(filter90AndAbove(submissions));
 
-// God hath not seen thine R A G E like my furious and severe lack of understanding of what I am doing with these.
 const createRange = (start, end) => {
-  const rangeArray = [start];
-  // WHILE the ending object/item is larger than the starting item
-  // Plus one to the starting item, and slap it on the END of the new array....
-  // I know I need to use the .reduce() method here for pv and cv so I can go "if the cv value === end then ABORT MISSION"
-  // but how do I understand how to do that... MATHMATICALLY/SYSTEMATICALLY
-  while (end > start) {
-    rangeArray.push(start++);
+  const rangeArray = [];
+  for (let i = start; i <= end; i++) {
+    rangeArray.push(i);
   }
+  return rangeArray;
+  // whilte(start <= end) {rangeArray.push(start++);} return rangeArray;
 };
 console.log(createRange(2, 5));
 
-// Despite trying my best, I still can't get shit in detroit /ba-dum-tss
-const countingArray = ["a", "c", "a", "b", "a", "b"];
 const countElements = (array) => {
-  array.findIndex((item) => {
-    // my beautiful counters
-    let aNum = 0;
-    let bNum = 0;
-    let cNum = 0;
-    if (item === "a") {
-      aNum++;
-    } else if (item === "b") {
-      bNum++;
+  const newObject = {};
+  array.forEach((item) => {
+    if (newObject[item]) {
+      newObject[item]++;
     } else {
-      cNum++;
+      newObject[item] = 1;
     }
-    // I'm hoping I just put the return wrong or something and this doesn't end up neeing the .map() method
-    return [`a: ${aNum}`, `b: ${bNum}`, `c: ${cNum}`];
   });
+  return newObject;
 };
-console.log(countElements(countingArray));
+console.log(countElements(["a", "c", "a", "b", "a", "b"]));
